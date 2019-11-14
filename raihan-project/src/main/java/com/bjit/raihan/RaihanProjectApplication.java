53a5988fc59c5541a16afb9744640a23aa5f7f01
Raihan Al-Mamun <raihan@uxd.co.jp>
2019-11-14 10:35:49 +0600

    project init

---
+++ raihan-project/src/main/java/com/bjit/raihan/RaihanProjectApplication.java
@@ -0,0 +1,13 @@
+package com.bjit.raihan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaihanProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaihanProjectApplication.class, args);
+	}
+
+}
